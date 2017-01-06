558f015e3bd1c499350297daf915912eceeec6fb
Dominik Schmidt <nick@kreativgebiet.com>
2017-01-06 11:22:02 +0100

    remove SetUserByToken again

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 # encoding: utf-8
 class ApplicationController < ActionController::Base
-  include DeviseTokenAuth::Concerns::SetUserByToken
+  # include DeviseTokenAuth::Concerns::SetUserByToken
 
   before_action :configure_permitted_parameters, if: :devise_controller?
 
